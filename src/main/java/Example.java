50a8307c06ac3136e4cb8b6e0c237b67b04e873b
fransiscos <frans@pohondana.id>
2021-07-12 09:02:09 +0700

    sasadasd

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World - v3sssssssss testtt sss!";
+        return "Hello World - v3ssssssssssssss testtt sss!";
     }
 
     public static void main(String[] args) throws Exception {
